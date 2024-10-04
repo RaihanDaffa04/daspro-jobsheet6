95872edde67647c3c10af15e4983b89242b1c072
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2024-10-04 14:53:07 +0800

    commit modifikasi no 4

--- Pemilihan2Percobaan124.java
+++ Pemilihan2Percobaan124.java
@@ -9,11 +9,13 @@ public class Pemilihan2Percobaan124 {
         tahun = input24.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
+            if ((tahun % 100) == 0) {
+                if ((tahun % 400) == 0) {
                 System.out.println("Tahun kabisat");
+                } else System.out.println("Bukan tahun kabisat");
             } else
-                System.out.println("Bukan tahun kabisat");
+                System.out.println("Tahun kabisat");
         } else
             System.out.println("Bukan tahun kabisat");
     }
-}
+    }
